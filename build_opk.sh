8ffa4ffc87c39de6c608f7b5563cc5038f8a2e77
Vincent-FK <vincent.buso@funkey-project.com>
2021-02-12 18:58:03 +0100

    rm -f instead of rm in build_opk.sh

--- build_opk.sh
+++ build_opk.sh
@@ -7,7 +7,7 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-rm sm64-port-"$1".opk
+rm -f sm64-port-"$1".opk
 convert textures/segment2/segment2.05A00.rgba16.png -resize 32x32! build/icon.png
 #mksquashfs build/us_pc/sm64.us.f3dex2e ./run.sh build/icon.png default."$1".desktop sm64-port-"$1".opk 
 mksquashfs build/us_pc/sm64.us.f3dex2e build/icon.png default."$1".desktop sm64-port-"$1".opk 
